7def3d122f2f21a7403e7731004274edc9603a54
Ajesh DS <ajeshd17@gmail.com>
2019-03-03 23:28:59 +0530

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../styles/App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+			<h1>Quick Entry!</h1>
+			<p>Entry management system for TEC</p>
+			</div>
+		);
+	}
+}
+
+export default App;
