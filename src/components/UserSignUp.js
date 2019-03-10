bb38ec454a79fdb7f6b485a0b01d42c2f01af521
Ajesh DS <ajeshd17@gmail.com>
2019-03-10 21:59:07 +0530

    Complete Basic USer Sign Up form

--- src/components/UserSignUp.js
+++ src/components/UserSignUp.js
@@ -1,10 +1,86 @@
 import React, { Component } from 'react';
+import '../styles/userauth.css';
 
 class UserSignUp extends Component {
+
+    state = {
+        username: "",
+        fullname: "",
+        email: "",
+        password: "",
+        tecid: ""
+    }
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
     render() {
+        const {fullname, username, email, password, tecid} = this.state;
         return (
-            <div>
-                User Sign Up Form
+            <div className="signup">
+                <div className="container">
+                    <h1 className="gradient-text">Join Quick-Entry</h1>
+                    <form className="signup-form">
+                        <label>
+                            Full Name
+                            <input 
+                            type="text" 
+                            name="fullname" 
+                            placeholder="John Doe"
+                            value={fullname}
+                            required
+                            onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Username
+                            <input 
+                            type="text" 
+                            name="username" 
+                            value={username}
+                            placeholder="johndoe123"
+                            required
+                            onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Email
+                            <input 
+                            type="email" 
+                            name="email" 
+                            value={email}
+                            required
+                            placeholder="john@doe.com"
+                            onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            TEC ID
+                            <input 
+                            type="text" 
+                            name="tecid" 
+                            value={tecid}
+                            required
+                            placeholder="Your ID, for example: TU3F1718076"
+                            onChange={this.handleChange}/>
+                        </label>
+                        <label>
+                            Password
+                            <input 
+                            type="password" 
+                            name="password" 
+                            value={password}
+                            required
+                            placeholder="Strong Password"
+                            autoComplete="new-password"
+                            onChange={this.handleChange}/>
+                        </label>
+                        <button className="button">Submit</button>
+                    </form>
+                </div>
             </div>
         )
     }
