9d7d17f21ba8b35d8195ca1b8a7b059a78de6b59
Ajesh DS <ajeshd17@gmail.com>
2019-03-11 22:28:51 +0530

    Remove Snackbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Snackbar from '@material-ui/core/Snackbar';
-import SnackbarContent from '@material-ui/core/SnackbarContent';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import { configStore } from '../store';
@@ -11,21 +10,9 @@ const store = configStore();
 
 class App extends Component {
 	render() {
-		const state = store.getState();
 		return (
 			<Provider store={store}>
 				<AppRouter />
-				{state.errors.message && 
-					<Snackbar
-						anchorOrigin={{
-							vertical: 'bottom',
-							horizontal: 'right'
-						}}
-						open={true}
-						autoHideDuration={5000}
-						message={state.errors.message}
-					/>
-				}
 			</Provider>
 		);
 	}
