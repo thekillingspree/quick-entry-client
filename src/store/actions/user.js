fce12d18baf2f3623438ae8ddd161f8c78f6c5d0
Ajesh DS <ajeshd17@gmail.com>
2019-03-12 06:41:45 +0530

    Add User Sign In Action

--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -14,8 +14,36 @@ export const signUpUser = user => {
     return dispatch => {
         return new Promise((resolve, reject) => {
             axios.post(`${API_URL}/users/signup`, user).then((res) => {
-                console.log(res);
-                //dispatch(setCurrentUser(user))
+                const data = res.data;
+                const u = {
+                    ...data.result,
+                    token: data.token
+                }
+                console.log(u)
+                dispatch(setCurrentUser(u))
+                dispatch(removeError())
+                resolve();
+            }).catch((err) => {
+                const msg = err.response.data.error;
+                console.log(msg);
+                dispatch(addError(msg))
+                reject();
+            });
+        });
+    }
+}
+
+export const signInUser = user => {
+    return dispatch => {
+        return new Promise((resolve, reject) => {
+            axios.post(`${API_URL}/users/login`, user).then((res) => {
+                const data = res.data;
+                const u = {
+                    ...data.result,
+                    token: data.token
+                }
+                console.log(u)
+                dispatch(setCurrentUser(u))
                 dispatch(removeError())
                 resolve();
             }).catch((err) => {
