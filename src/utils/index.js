6f021f6c20c1f24e040933f7f6fba0b9e6a38b63
Ajesh DS <ajeshd17@gmail.com>
2019-03-13 09:12:01 +0530

    Add endpoint uris

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
-export const API_URL = "https://quick-entry.herokuapp.com/api";
+export const API_URL = 'https://quick-entry.herokuapp.com/api';
+
+export const USER_LOGIN = '/users/login';
+export const USER_SIGN_UP = '/users/signup';
+export const PROFILE = '/users/profile'; //GET
+
+export const ADMIN_LOGIN = '/admin/login';
+export const ADMIN_SIGN_UP = '/admin/signup';
+export const ADMIN_GET_ROOMS = '/admin/rooms'; //GET
+export const ADMIN_NEW_ROOM = '/rooms/new';
 
 export const authMapStateToProps = state => (
     {
