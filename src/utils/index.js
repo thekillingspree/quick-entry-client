a5aa33b702ee6fe23f2bf1d5b713bc8926e43a33
Ajesh DS <ajeshd17@gmail.com>
2019-03-13 09:02:54 +0530

    Utility fuction for making API calls

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const API_URL = "https://quick-entry.herokuapp.com/api";
 
 export const authMapStateToProps = state => (
@@ -5,3 +7,13 @@ export const authMapStateToProps = state => (
         error: state.errors.message
     }
 )
+
+export const apiCall = (method, data, url) => {
+    return new Promise((resolve, reject) => {
+        axios[method](`${API_URL}${url}`, data).then((result) => {
+            resolve(result.data)
+        }).catch((err) => {
+            reject(err.response.data.error);
+        });
+    });
+}
