60e1f5f74ff70d47df40b9f8ec7c5ecdf916b6bd
Ajesh DS <ajeshd17@gmail.com>
2019-03-15 23:05:54 +0530

    Add Loader

--- src/components/RoomDetails.js
+++ src/components/RoomDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { getRoomDetails } from '../store/actions/admin';
 
 class RoomDetails extends Component {
@@ -14,8 +15,9 @@ class RoomDetails extends Component {
         const { uid, rid} = this.props.match.params;
         this.props.dispatch(getRoomDetails(uid, rid)).then((room) => {
             console.log(room);
+            this.setState({room, loading: false})
         }).catch((err) => {
-            console.log(err);
+            this.props.history.push('/')
         }); 
     }
 
@@ -24,11 +26,13 @@ class RoomDetails extends Component {
         const { room, loading } = this.state;
         return (
             <div className="dashboard">
-                Rooms info
                 {(room && !loading) && <div className="room-details">
                     <h1 className="gradient-text">
                         { room.name }
                     </h1>
+                </div>}
+                {loading && <div className="center all loading">
+                    <CircularProgress size={70} thickness={5} className="progress"/>
                 </div>}
             </div>
         )
