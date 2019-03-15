10476e1022a0624d2742419efca5133159e9f0fc
Ajesh DS <ajeshd17@gmail.com>
2019-03-15 22:54:00 +0530

    Handle Click function

--- src/components/RoomDisplay.js
+++ src/components/RoomDisplay.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import GroupIcon from '@material-ui/icons/Group';
 import CurrentIcon from '@material-ui/icons/Timeline';
 
 const RoomDisplay = props => {
-    const {name, capacity, roomnumber, current} = props.room;
+    const  { uid, history, room } = props;
+    const {name, capacity, roomnumber, current} = room;
+    
+    const handleClick = () => {
+        let {_id} = room;
+        _id = _id['$oid']
+        history.push(`/admin/${uid}/room/${_id}`)
+    }
     return (
-        <div className="room-display gradient-bg shadow">
+        <div onClick={handleClick} className="room-display gradient-bg shadow">
             <div className="top">
                 <h1>{name}</h1>
                 <p>{roomnumber}</p>
@@ -18,4 +26,4 @@ const RoomDisplay = props => {
     )
 }
 
-export default RoomDisplay
+export default withRouter(RoomDisplay)
