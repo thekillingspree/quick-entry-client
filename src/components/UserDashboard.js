75a31a70ffc0191d1024f1362eb6aee1f7309149
Ajesh DS <ajeshd17@gmail.com>
2019-03-20 23:58:46 +0530

    Change Selected menu

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -10,13 +10,16 @@ import MenuItem from '@material-ui/core/MenuItem';
 import AddIcon from '@material-ui/icons/AddCircleOutline';
 import CircularProgress from '@material-ui/core/CircularProgress'; 
 import { authMapStateToProps } from '../utils';
+import { getHistory } from '../store/actions/user';
 
 class UserDashboard extends Component {
 
     state = {
         color: '#9a81d4',
         colorAnchor: null,
+        colorSelected: 0,
         size: 300,
+        sizeSelected: 0,
         sizeAnchor: null,
     }
 
@@ -35,8 +38,8 @@ class UserDashboard extends Component {
         this.setState({sizeAnchor: null});
     }   
 
-    selectSize = size => {
-        this.setState({size, sizeAnchor: null});
+    selectSize = (size, sizeSelected) => {
+        this.setState({size, sizeSelected, sizeAnchor: null});
     }
 
     colorClick = e => {
@@ -47,17 +50,18 @@ class UserDashboard extends Component {
         this.setState({colorAnchor: null});
     }   
 
-    selectColor = color  => {
-        this.setState({color, colorAnchor: null});
+    selectColor = (color, colorSelected)  => {
+        this.setState({color, colorSelected, colorAnchor: null});
     }
 
     componentDidMount() {
         const { _id } = this.props.user;
+        this.props.dispatch(getHistory(_id['$oid']));
     }
 
     render() {
         let {fullname, email, username, tecid, _id} = this.props.user;
-        const {color, size, sizeAnchor, colorAnchor} = this.state;
+        const {color, size, sizeAnchor, colorAnchor, colorSelected, sizeSelected} = this.state;
         console.log(fullname)
         return (
             <div className="dashboard">
@@ -82,36 +86,36 @@ class UserDashboard extends Component {
                             open={!!sizeAnchor}
                             onClose={this.sizeClose}
                         >
-                            <MenuItem onClick={() => this.selectSize(300)}>300x300</MenuItem>
-                            <MenuItem onClick={() => this.selectSize(500)}>500x500</MenuItem>
-                            <MenuItem onClick={() => this.selectSize(800)}>800x800</MenuItem>
+                            <MenuItem selected={sizeSelected == 0} onClick={() => this.selectSize(300, 0)}>300x300</MenuItem>
+                            <MenuItem selected={sizeSelected == 1} onClick={() => this.selectSize(500, 1)}>500x500</MenuItem>
+                            <MenuItem selected={sizeSelected == 2} onClick={() => this.selectSize(800, 2)}>800x800</MenuItem>
                         </Menu>
                         <Menu id="color"
                             anchorEl={colorAnchor}
                             open={!!colorAnchor}
                             onClose={this.colorClose}
                         >
-                            <MenuItem onClick={() => this.selectColor('#9a81d4')}>
+                            <MenuItem selected={colorSelected == 0} onClick={() => this.selectColor('#9a81d4', 0)}>
                                 <span className="color-box" style={{background: '#9a81d4'}}></span>
                                 Quick-Entry
                             </MenuItem>
-                            <MenuItem onClick={() => this.selectColor('#fc5c65')}>
+                            <MenuItem selected={colorSelected == 1} onClick={() => this.selectColor('#fc5c65', 1)}>
                                 <span className="color-box" style={{background: '#fc5c65'}}></span>
                                 Fusion Red
                             </MenuItem>
-                            <MenuItem onClick={() => this.selectColor('#26de81')}>
+                            <MenuItem selected={colorSelected == 2} onClick={() => this.selectColor('#26de81', 2)}>
                                 <span className="color-box" style={{background: '#26de81'}}></span>
                                 Grass Green
                             </MenuItem>
-                            <MenuItem onClick={() => this.selectColor('#3867d6')}>
+                            <MenuItem selected={colorSelected == 3} onClick={() => this.selectColor('#3867d6', 3)}>
                                 <span className="color-box" style={{background: '#3867d6'}}></span>
                                 Royal Blue
                             </MenuItem>
-                            <MenuItem onClick={() => this.selectColor('#2d3436')}>
+                            <MenuItem selected={colorSelected == 4} onClick={() => this.selectColor('#2d3436', 4)}>
                                 <span className="color-box" style={{background: '#2d3436'}}></span>
                                 Dracula
                             </MenuItem>
-                            <MenuItem onClick={() => this.selectColor('#e84393')}>
+                            <MenuItem selected={colorSelected == 5} onClick={() => this.selectColor('#e84393', 5)}>
                                 <span className="color-box" style={{background: '#e84393'}}></span>
                                 Prunus Pink
                             </MenuItem>
